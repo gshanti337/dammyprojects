a85e8bef87719c45dc7f3f6c90a080a323307b06
gshanti337 <59365729+gshanti337@users.noreply.github.com>
2019-12-30 19:51:26 +0530

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.box1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.box9 = new System.Windows.Forms.Button();
+            this.box6 = new System.Windows.Forms.Button();
+            this.box3 = new System.Windows.Forms.Button();
+            this.box8 = new System.Windows.Forms.Button();
+            this.box7 = new System.Windows.Forms.Button();
+            this.box = new System.Windows.Forms.Button();
+            this.box4 = new System.Windows.Forms.Button();
+            this.box2 = new System.Windows.Forms.Button();
+            this.player_1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.player_2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // box1
+            // 
+            this.box1.BackColor = System.Drawing.Color.White;
+            this.box1.Location = new System.Drawing.Point(14, 6);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(75, 64);
+            this.box1.TabIndex = 0;
+            this.box1.UseVisualStyleBackColor = false;
+            this.box1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Controls.Add(this.box9);
+            this.panel1.Controls.Add(this.box6);
+            this.panel1.Controls.Add(this.box3);
+            this.panel1.Controls.Add(this.box8);
+            this.panel1.Controls.Add(this.box7);
+            this.panel1.Controls.Add(this.box);
+            this.panel1.Controls.Add(this.box4);
+            this.panel1.Controls.Add(this.box2);
+            this.panel1.Controls.Add(this.box1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(51, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 199);
+            this.panel1.TabIndex = 1;
+            // 
+            // box9
+            // 
+            this.box9.BackColor = System.Drawing.Color.White;
+            this.box9.Location = new System.Drawing.Point(155, 126);
+            this.box9.Name = "box9";
+            this.box9.Size = new System.Drawing.Size(75, 64);
+            this.box9.TabIndex = 8;
+            this.box9.UseVisualStyleBackColor = false;
+            this.box9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // box6
+            // 
+            this.box6.BackColor = System.Drawing.Color.White;
+            this.box6.Location = new System.Drawing.Point(155, 66);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(75, 64);
+            this.box6.TabIndex = 7;
+            this.box6.UseVisualStyleBackColor = false;
+            this.box6.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // box3
+            // 
+            this.box3.BackColor = System.Drawing.Color.White;
+            this.box3.Location = new System.Drawing.Point(155, 6);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(75, 64);
+            this.box3.TabIndex = 6;
+            this.box3.UseVisualStyleBackColor = false;
+            this.box3.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // box8
+            // 
+            this.box8.BackColor = System.Drawing.Color.White;
+            this.box8.Location = new System.Drawing.Point(86, 126);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(74, 64);
+            this.box8.TabIndex = 5;
+            this.box8.UseVisualStyleBackColor = false;
+            this.box8.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // box7
+            // 
+            this.box7.BackColor = System.Drawing.Color.White;
+            this.box7.Location = new System.Drawing.Point(15, 126);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(74, 64);
+            this.box7.TabIndex = 4;
+            this.box7.UseVisualStyleBackColor = false;
+            this.box7.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // box
+            // 
+            this.box.BackColor = System.Drawing.Color.White;
+            this.box.Location = new System.Drawing.Point(85, 66);
+            this.box.Name = "box";
+            this.box.Size = new System.Drawing.Size(75, 64);
+            this.box.TabIndex = 3;
+            this.box.UseVisualStyleBackColor = false;
+            this.box.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // box4
+            // 
+            this.box4.BackColor = System.Drawing.Color.White;
+            this.box4.Location = new System.Drawing.Point(14, 66);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(75, 64);
+            this.box4.TabIndex = 2;
+            this.box4.UseVisualStyleBackColor = false;
+            this.box4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // box2
+            // 
+            this.box2.BackColor = System.Drawing.Color.White;
+            this.box2.Location = new System.Drawing.Point(85, 6);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(75, 64);
+            this.box2.TabIndex = 1;
+            this.box2.UseVisualStyleBackColor = false;
+            this.box2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // player_1
+            // 
+            this.player_1.AutoSize = true;
+            this.player_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_1.Location = new System.Drawing.Point(48, 244);
+            this.player_1.Name = "player_1";
+            this.player_1.Size = new System.Drawing.Size(0, 31);
+            this.player_1.TabIndex = 6;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(326, 228);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(108, 47);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(69, 244);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 31);
+            this.result.TabIndex = 4;
+            // 
+            // player_2
+            // 
+            this.player_2.AutoSize = true;
+            this.player_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player_2.Location = new System.Drawing.Point(180, 244);
+            this.player_2.Name = "player_2";
+            this.player_2.Size = new System.Drawing.Size(0, 31);
+            this.player_2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(477, 328);
+            this.Controls.Add(this.player_2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.player_1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button box1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button box9;
+        private System.Windows.Forms.Button box6;
+        private System.Windows.Forms.Button box3;
+        private System.Windows.Forms.Button box8;
+        private System.Windows.Forms.Button box7;
+        private System.Windows.Forms.Button box;
+        private System.Windows.Forms.Button box4;
+        private System.Windows.Forms.Button box2;
+        private System.Windows.Forms.Label player_1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label player_2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0,i=0;
+        Boolean t;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+        public bool selectbutton(Button b1,Button b2,Button b3)
+        {
+        
+            if (b1.Text == "X" && b2.Text == "X" && b3.Text == "X" ||
+                b1.Text == "0" && b2.Text == "0" && b3.Text == "0")
+            {
+                t = true;
+                Button[] arratbutton = new Button[9] { box1, box2, box, box4, box8, box7, box3, box6, box9 };
+                for (int p = 0; p < 9; p++)
+                {
+                    arratbutton[p].Enabled = false;
+                }
+                result.Text = "You won the game";
+                result.ForeColor = Color.Green;
+                player_1.ResetText();
+                player_2.ResetText();
+            }
+            else
+            {
+
+                t = false;
+            }
+            return t;
+        }
+
+        public void selectcolor(Button b1, Button b2, Button b3)
+        {
+            b1.BackColor = Color.LawnGreen;
+            b2.BackColor = Color.LawnGreen;
+            b3.BackColor = Color.LawnGreen;
+        }
+        public void drawgame(int i)//method for when draw the game
+        {
+            if (i == 8)
+            {
+                if (t==false)
+                {
+                    result.Text = "draw game";
+                    result.ForeColor = Color.Red;
+                    player_1.ResetText();
+                    player_2.ResetText();
+                }
+            }
+        }
+        public void display(Button b)
+        {
+            if (counter == 0)
+            {
+                player_1.Text = "player X";
+                player_1.ForeColor = Color.RoyalBlue;
+                player_2.ForeColor = Color.Black;
+             
+                b.Text = "X";
+                counter++;
+                b.Enabled = false;
+            }
+            else if (counter == 1)
+            {
+                player_2.Text = "player O";
+                player_2.ForeColor = Color.RoyalBlue;
+                player_1.ForeColor = Color.Black;
+                b.Text = "0";
+                counter--;
+                b.Enabled = false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawgame(i++);
+            display(box1);
+            if (selectbutton(box1, box2, box3))
+            {
+                selectcolor(box1, box2, box3);
+            }
+            else if (selectbutton(box1, box, box9))
+            {
+                selectcolor(box1, box, box9);
+            }
+            else if (selectbutton(box1, box4, box7))
+            {
+                selectcolor(box1, box4, box7);
+            }
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            display(box2);
+            if (selectbutton(box1, box2, box3))
+            {
+                selectcolor(box1, box2, box3);
+            }
+            else if (selectbutton(box2, box, box8))
+            {
+                selectcolor(box2, box, box8);
+            }
+            drawgame(i++);
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            display(box3);
+            if (selectbutton(box3, box, box7))
+            {
+                selectcolor(box3, box, box7);
+            }
+            else if (selectbutton(box1, box2, box3))
+            {
+                selectcolor(box1, box2, box3);
+            }
+            else if (selectbutton(box3, box6, box9))
+            {
+                selectcolor(box3, box6, box9);
+            }
+            drawgame(i++);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            display(box4);
+            if (selectbutton(box1, box4, box7))
+            {
+                selectcolor(box1, box4, box7);
+            }
+            else if (selectbutton(box4, box, box6))
+            {
+                selectcolor(box4, box, box6);
+            }
+            drawgame(i++);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            display(box);
+            if (selectbutton(box1, box, box9))
+            {
+                selectcolor(box1, box, box9);
+            }
+            else if (selectbutton(box3, box, box7))
+            {
+                selectcolor(box3, box, box7);
+            }
+            else if (selectbutton(box4, box, box6))
+            {
+                selectcolor(box4, box, box6);
+            }
+            else if (selectbutton(box2, box, box8))
+            {
+                selectcolor(box2, box, box8);
+            }
+            drawgame(i++);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            display(box6);
+            if (selectbutton(box3, box6, box9))
+            {
+                selectcolor(box3, box6, box9);
+            }
+            else if (selectbutton(box4, box, box6))
+            {
+                selectcolor(box4, box, box6);
+            }
+            drawgame(i++);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            display(box7);
+            if (selectbutton(box1, box4, box7))
+            {
+                selectcolor(box1, box4, box7);
+            }
+            else if (selectbutton(box3, box, box7))
+            {
+                selectcolor(box3, box, box7);
+            }
+            else if (selectbutton(box7, box8, box9))
+            {
+                selectcolor(box7, box8, box9);
+            }
+            drawgame(i++);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            display(box8);
+            if (selectbutton(box7, box8, box9))
+            { 
+            selectbutton(box7, box8, box9);
+            }
+            else if (selectbutton(box2, box, box8))
+            {
+                selectcolor(box2, box, box8);
+            }
+            drawgame(i++);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            display(box9);
+            if (selectbutton(box1, box, box9))
+            {
+                selectcolor(box1, box, box9);
+            }
+            else if (selectbutton(box3, box6, box9))
+            {
+                selectcolor(box3, box6, box9);
+            }
+            else if (selectbutton(box7, box8, box9))
+            {
+                selectcolor(box7, box8, box9);
+            }
+            drawgame(i++);
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            i = 0;
+            player_1.ResetText();
+            player_2.ResetText();
+            result.ResetText();
+            Button[] arratbutton = new Button[9] { box1, box2, box, box4, box8, box7, box3, box6, box9 };
+            for (int l = 0; l < 9; l++)
+            {
+                arratbutton[l].Enabled = true;
+                arratbutton[l].Text = "";
+                arratbutton[l].BackColor = Color.White;
+            }
+        }
+    }
+}
